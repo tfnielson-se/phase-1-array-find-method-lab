0f9ade25fc569078437b3ae698787018ebb32270
Thomas <tfnielson.se@gmail.com>
2022-12-18 09:08:34 -0700

    commit tree c09d0099226588e4592313ee0d7843285ca5c2bf

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // code your solution here
+const record = [
+  { year: "2015", result: "W"},
+  { year: "2014", result: "N/A"},
+  { year: "2013", result: "L"},
+]
+
+function superbowlWin(record){
+	let result = record.find(record => record.result === 'W')
+	return result ? result.year : undefined
+};
+
+console.log(superbowlWin(record));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// superbowlWin = (record) => {
+//     const result = record.find( record => record.result === "W")
+//     return !!result ? result.year : undefined;
+// }
+
+// function superbowlWin (record) {
+//   let result = record.find(record.result === 'W');
+//   if (result = true) {
+//     console.log(record.year);
+//   };
+
+// }
